6cc4e5ba76a7ea612ca578500c12e2d4d6c7d28d
YGajender <gajenderdahina@gmail.com>
2024-11-07 12:33:56 +0530

    changes in schema

--- models/fsl.js
+++ models/fsl.js
@@ -1,33 +1,32 @@
 import mongoose from "mongoose";
 
 const FslSchema = new mongoose.Schema({
-    personalDetails: {
-        name: { type: String, required: true },
-        email: { type: String, required: true },
-        phone: { type: Number, required: true },
-        dateOfBirth: { type: Date, required: true },
-        gender: { type: String, required: true },
-        aadharCardFront: { type: String, required: true },
-        aadharCardBack: { type: String, required: true }
-    },
-    parent_Guardian_SpouseDetails: {
-        name: { type: String, required: true },
-        phoneNumber: { type: Number, required: true }
-    },
-    Educational_Details: {
-        Are_You_a: {type: String,required: true},
-        Last_Attained_Qualification: {type: String,required: true},
-        Year: {type: Number,required: true},
-        College_University: {type: String,required: true}
-    },
-    Course_Details: {
-        Course: {type: String,required: true},
-        How_you_came_to_know_about_us: {type: String,required: true}
-    },
-    Terms_condition:{
-        type: Boolean,
-        required: true
-    }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: Number, required: true },
+    dob: { type: Date, required: true },
+    gender: { type: String, required: true },
+    aadharFront: { type: String, required: true },
+    aadharBack: { type: String, required: true },
+
+    laddress: { type: String, required: true },
+    paddress: { type: String },
+
+    fname: { type: String, required: true },
+    fphone: { type: Number, required: true },
+
+    areYoua: { type: String, required: true },
+    qualification: { type: String },
+    qualificationYear: { type: Number },
+    college: { type: String },
+    designation: { type: String },
+    company: { type: String },
+
+    Course: { type: String, required: true },
+    knowus: { type: String, required: true },
+    friendname: { type: String },
+
+    Terms_condition: { type: Boolean, required: true }
 });
 const FslModel = mongoose.model("fsl", FslSchema);
 export default FslModel;
